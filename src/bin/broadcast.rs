15381e9ef37e58142798fd9964ad216477a80312
daalfox <daalfox@gmail.com>
2024-10-09 15:28:27 +0330

    Make broadcast efficient (challenge #3d and #3e)

--- src/bin/broadcast.rs
+++ src/bin/broadcast.rs
@@ -1,21 +1,36 @@
+use core::panic;
 use std::{
     collections::{HashMap, HashSet},
     io::stdin,
+    sync::{Arc, Mutex},
+    thread,
+    time::Duration,
 };
 
-use loom::{request::Request, response::Response, Node, NodeId};
+use loom::{
+    request::{self, Request},
+    response::Response,
+    Node, NodeId,
+};
 use serde::{Deserialize, Serialize};
 use serde_json::{de, json};
 
 fn main() -> anyhow::Result<()> {
-    let mut node = BroadcastNode::init();
+    let node = Arc::new(Mutex::new(BroadcastNode::init()));
+
+    let gossip_node = node.clone();
+    thread::spawn(move || loop {
+        thread::sleep(Duration::from_millis(150));
+        gossip_node.lock().unwrap().gossip();
+    });
 
     let lines = stdin().lines();
     for line in lines {
         let request: Request<Req> = de::from_str(&line.unwrap())?;
-        let response = node.handle(request);
 
-        println!("{}", json!(response));
+        if let Some(response) = node.lock().unwrap().handle(request) {
+            println!("{}", json!(response));
+        }
     }
 
     Ok(())
@@ -27,42 +42,80 @@ struct BroadcastNode {
     node_ids: Vec<NodeId>,
     topology: HashMap<NodeId, Vec<NodeId>>,
     messages: HashSet<usize>,
+    known: HashMap<NodeId, HashSet<usize>>,
 }
 
 impl Node for BroadcastNode {
     fn from_init(node_id: NodeId, node_ids: Vec<NodeId>) -> Self {
+        let mut known = HashMap::new();
+        for node in node_ids.iter() {
+            known.insert(node.clone(), HashSet::new());
+        }
         BroadcastNode {
             node_id,
             node_ids,
             topology: HashMap::new(),
             messages: HashSet::new(),
+            known,
         }
     }
 }
 
 impl BroadcastNode {
-    fn handle(&mut self, request: Request<Req>) -> Response<Res> {
+    fn handle(&mut self, request: Request<Req>) -> Option<Response<Res>> {
         match request.body.payload {
             Req::Broadcast { message } => {
                 self.messages.insert(message);
-                request.into()
+                Some(request.into())
             }
             Req::Read => {
                 let mut response: Response<Res> = request.into();
                 response.body.payload = Res::ReadOk {
                     messages: self.messages.clone(),
                 };
-                response
+                Some(response)
             }
-            Req::Topology { ref topology } => {
-                self.topology = topology.clone();
-                request.into()
+            Req::Topology { .. } => {
+                self.topology = star_topology(&self.node_ids);
+                Some(request.into())
             }
+            Req::Gossip { ref messages } => {
+                self.messages.extend(messages);
+                self.known.get_mut(&request.src).unwrap().extend(messages);
+                None
+            }
+        }
+    }
+    fn gossip(&self) {
+        let messages = self.messages.clone();
+        let neighborhood = self
+            .topology
+            .get(&self.node_id)
+            .unwrap_or_else(|| panic!("{} has no topology", &self.node_id));
+        for n in neighborhood {
+            let known_to_n = self
+                .known
+                .get(n)
+                .unwrap_or_else(|| panic!("{n} is an unknown node"));
+            let unknown_to_n = messages.difference(known_to_n).copied().collect();
+
+            let req = Request {
+                src: self.node_id.to_string(),
+                dest: n.to_string(),
+                body: request::Body {
+                    id: None,
+                    payload: Req::Gossip {
+                        messages: unknown_to_n,
+                    },
+                },
+            };
+
+            println!("{}", json!(req));
         }
     }
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 #[serde(tag = "type")]
 #[serde(rename_all = "snake_case")]
 #[allow(dead_code)]
@@ -73,6 +126,9 @@ enum Req {
     Read,
     Topology {
         topology: HashMap<NodeId, Vec<NodeId>>,
+    },
+    Gossip {
+        messages: HashSet<usize>,
     },
 }
 
@@ -93,6 +149,17 @@ impl From<Req> for Res {
                 messages: HashSet::new(),
             },
             Req::Topology { .. } => Self::TopologyOk,
+            Req::Gossip { .. } => panic!("can't reply to gossip messages"),
         }
     }
+}
+
+/// make a star topology from the given `nodes`
+fn star_topology(nodes: &[NodeId]) -> HashMap<NodeId, Vec<NodeId>> {
+    let mut result = HashMap::new();
+    result.insert(nodes[0].clone(), nodes[1..].to_vec());
+    for node in nodes[1..].iter() {
+        result.insert(node.to_string(), nodes[0..1].to_vec());
+    }
+    result
 }
